166930978c4fedce0a57fdd2f3b24e63c9ed9113
ggae0723 <72652893+ggae0723@users.noreply.github.com>
2020-12-09 19:33:04 +0900

    Update 시계.js

--- 시계.js
+++ 시계.js
@@ -2,14 +2,20 @@
    document.body.append(사진)
    사진.src = 'https://t1.daumcdn.net/cfile/blog/994BB93F5AD305BF02?download';
    사진.width = 1980
-   사진.height = 335
+   사진.height = 300
    document.body.style.backgroundColor = 'black'
    var 시간 = document.createElement('span')
    document.body.append(시간)
-   시간.style.fontSize = 130
-   시간.style.marginLeft = 55
+   시간.style.fontSize = 135
+   시간.style.marginLeft = 444
    시간.style.color = 'white'
    시간.style.fontFamily = 'Cursive'
+   var 시간2 = document.createElement('div')
+   document.body.append(시간2)
+   시간2.style.fontSize = 135
+   시간2.style.marginLeft = 325
+   시간2.style.color = 'white'
+   시간2.style.fontFamily = 'Cursive'
    var 년도1 = null 
    var 년도2 = null
    var 년도3 = null
@@ -24,8 +30,40 @@
    var 초1 = null
    var 초2 = null
    var 초 = null
+   var 날1;
+   var 날2;
+   var 월;
    var r = 0
    setInterval(function callback(e){
+ 월 = Date()[4] + Date()[5] + Date()[6]
+   if(월 === "Jan") {
+      월 = '1'
+} else if (월 === 'Feb') {
+   월 = '2'
+} else if (월 === 'Mar') {
+   월 = '3'
+} else if (월 === 'Apr') {
+   월 = '4'
+} else if (월 === 'May') {
+   월 = '5'
+} else if (월 === 'Jun') {
+   월 = '6'
+} else if (월 === 'Jul') {
+   월 = '7'
+} else if (월 === 'Aug') {
+   월 = '6'
+} else if (월 === 'sep') {
+   월 = '8'
+} else if (월 === 'Oct') {
+   월 = '10'
+} else if (월 === 'Nov') {
+   월 = '11'
+} else if (월 === 'Dec') {
+   월 = '12'
+}
+      날1 = Date()[8]
+      날2 = Date()[9]
+      날 = 날1 + 날2
    년도1 = Date()[11]
    년도2 = Date()[12]
    년도3 = Date()[13]
@@ -60,6 +98,7 @@
    if (r === 0) {
       시간.textContent = 년도+'년' + ':' + 시+'시' + ':' + 분+'분' + ':' + 초+'초' + '(오전)'
    } else if(r === 1) {
-      시간.textContent = 년도+'년' + ':' + 시+'시' + ':' + 분+'분' + ':' + 초+'초' + '(오후)'
+      시간.textContent = 년도+'년' + 월 + '월'  + 날 + '일'
+      시간2.textContent = 시+'시' + ':' + 분+'분' + ':' + 초+'초' + '(오후)'
    }
    } ,  25)
